63961a9c18fd09ea375fb76b5aff979dfdfbefa4
SOUMYAJIT004 <ghoshsoumyajit2004@gmail.com>
2026-09-23 23:29:42 +0530

    Delete Folder 2/Wipro capstone project directory

--- Folder 2/Wipro capstone project/utils/config_reader.py
+++
@@ -1,8 +0,0 @@
-import configparser
-import os
-
-def get_config():
-    config = configparser.ConfigParser()
-    config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config', 'config.ini')
-    config.read(config_path)
-    return config
